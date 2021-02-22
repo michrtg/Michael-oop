294bd45fd483f32cdca3d3d0d23193c888432fcc
michrtg <77715698+michrtg@users.noreply.github.com>
2021-02-22 19:26:08 +0700

    Add files via upload

---
+++ input nilai.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char angka;
+	cout <<"Input sebuah nilai:";
+	cin >> angka;
+	
+	cout << "nilai yang di isi adalah :" << angka << endl;
+	return 0;
+}
